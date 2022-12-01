b76d036a73fd3672a39c6f045fe440ae6e34dc35
morris1961 <morris881961@gamil.com>
2022-12-01 20:46:06 +0800

    fix: unregister-command not working

--- unregister-command.js
+++ unregister-command.js
@@ -1,28 +1,27 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
+const { clientId, token, guildId } = require('./config.json');
 
 const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
   const args = process.argv.slice(2);
+
   try {
     const data = await rest.get(Routes.applicationGuildCommands(clientId, guildId))
-    const promises = [];
     for (const command of data) {
       // delete all commands if no provided args
       if (args.length == 0) {
         console.log(`delete ${command.name}`);
-        const deleteUrl = `${Routes.applicationGuildCommands(clientId)}/${command.id}`;
+        const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
         await rest.delete(deleteUrl);
       }
       // delete commands in args
-      else if (command.name in args) {
+      else if (args.includes(command.name)) {
         console.log(`delete ${command.name}`);
-        const deleteUrl = `${Routes.applicationGuildCommands(clientId)}/${command.id}`;
+        const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
         await rest.delete(deleteUrl);
       }
     }
-    return Promise.all(promises);
   } catch (error) {
     console.error(error);
   }
