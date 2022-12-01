a02ce5fdb91400d1f148727a26654b971c342a9e
morris1961 <morris881961@gamil.com>
2022-12-01 19:41:57 +0800

    refine unregister-command to delete specific command

--- unregister-command.js
+++ unregister-command.js
@@ -3,12 +3,28 @@ const { clientId, token } = require('./config.json');
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.get(Routes.applicationCommands(clientId))
-  .then(data => {
+(async () => {
+  const args = process.argv.slice(2);
+  try {
+    const data = await rest.get(Routes.applicationGuildCommands(clientId, guildId))
     const promises = [];
     for (const command of data) {
-      const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`;
-      promises.push(rest.delete(deleteUrl));
+      // delete all commands if no provided args
+      if (args.length == 0) {
+        console.log(`delete ${command.name}`);
+        const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`;
+        await rest.delete(deleteUrl);
+      }
+      // delete commands in args
+      else if (command.name in args) {
+        console.log(`delete ${command.name}`);
+        const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`;
+        await rest.delete(deleteUrl);
+      }
     }
     return Promise.all(promises);
-  });
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
